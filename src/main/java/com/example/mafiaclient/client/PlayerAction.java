8c3e276bb8097e0dec7c0af6a965b253c712f6ef
tzapadlinski <79110657+tzapadlinski@users.noreply.github.com>
2023-01-16 22:09:44 +0100

    Added player action

--- src/main/java/com/example/mafiaclient/client/PlayerAction.java
+++ src/main/java/com/example/mafiaclient/client/PlayerAction.java
@@ -3,4 +3,26 @@ package com.example.mafiaclient.client;
 public class PlayerAction {
     private int clientId;
     private int targetId;
+
+    public PlayerAction(int clientId, int targetId) {
+        this.clientId = clientId;
+        this.targetId = targetId;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public int getTargetId() {
+        return targetId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setTargetId(int targetId) {
+        this.targetId = targetId;
+    }
 }
+
